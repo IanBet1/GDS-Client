24ade8e59a2478817bd4bac31f59e9943efaf375
ianbet1 <ianbet1@192.168.0.10>
2016-11-24 21:08:53 -0200

    Implementação do relatório de saída de produtos.

---
+++ src/Controller/ControladorRelatorio.java
@@ -0,0 +1,65 @@
+package Controller;
+
+import Model.Relatorio;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class ControladorRelatorio {
+
+    private Relatorio relatorio;
+    private String erro;
+    private String endereco;
+    
+    public String getErro() {
+        return erro;
+    }
+
+    public void setErro(String erro) {
+        this.erro = erro;
+    }
+
+    public ControladorRelatorio() {
+        this.relatorio = new Relatorio();
+        this.endereco = "localhost";
+    }
+    
+    public List<Relatorio> relatorioSaida(Date datainic, Date datafim) {
+        List<Relatorio> relatorios = new ArrayList();
+        Relatorio r = new Relatorio();
+        r.setDescricao("");
+        r.setDate1(datainic);
+        r.setDate2(datafim);
+        r.setQtd1(0);
+        r.setQtd2(0);
+        
+        try (Socket clientSocket = new Socket(this.endereco, 1094)) {
+            System.out.println("Cliente solicitando relatório de saída de produtos na porta 1094.");
+
+            DataOutputStream outToServer1 = new DataOutputStream(clientSocket.getOutputStream());
+            DataInputStream inFromServer = new DataInputStream(clientSocket.getInputStream());
+
+            outToServer1.writeInt(13);
+            if (inFromServer.readBoolean() == true) {
+                ObjectOutputStream outToServer2 = new ObjectOutputStream(clientSocket.getOutputStream());
+                ObjectInputStream inFromServer2 = new ObjectInputStream(clientSocket.getInputStream());
+                outToServer2.writeObject(r);
+                relatorios = (List<Relatorio>) inFromServer2.readObject();
+                this.erro = inFromServer.readUTF();
+            }
+
+            outToServer1.close();
+            clientSocket.close();
+            return relatorios;
+        } catch (Exception e) {
+            this.erro = "Impossível fazer o relatório de saída!";
+            return null;
+        }
+    }
+
+}
